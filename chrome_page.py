d0870b297de5c9591ca204f6fff10b7afc1dcd90
lleonardx <lleonard.alves@gmail.com>
2024-05-08 09:55:20 -0400

    :sparkles: feat: Page Object function

---
+++ chrome_page.py
@@ -0,0 +1,15 @@
+import pyautogui
+import time
+
+class ChromePage:
+    def __init__(self):
+        self.window_key = "win"
+        self.chrome_app = "Google Chrome"
+        self.enter_key = "Enter"
+        self.wait_time = 1
+        
+    def open_chrome(self):
+        pyautogui.press(self.window_key)
+        pyautogui.write(self.chrome_app)
+        pyautogui.press(self.enter_key)
+        time.sleep(self.wait_time)
